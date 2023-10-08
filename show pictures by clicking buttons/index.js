31a19c358a33159038ef2bb8706310fef631cbbe
Fatih YAZICI <139749372+fatih-yazici@users.noreply.github.com>
2023-10-08 16:44:33 +0300

    Add files via upload

---
+++ show pictures by clicking buttons/index.js
@@ -0,0 +1,12 @@
+function image1(){
+    document.getElementById('resim').src='img/1.jpg';
+
+}
+
+function image2(){
+    document.getElementById('resim').src='img/2.jpg';
+}
+
+function image3(){
+    document.getElementById('resim').src='img/3.jpg';
+}
